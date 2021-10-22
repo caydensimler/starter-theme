f46ec9ad8a80fa2761091cfdf795da8140ef55c1
Cayden Simler <simler.cayden@gmail.com>
2021-10-22 12:52:40 -0700

    added missing span to post date

--- acf/content/post-date.php
+++ acf/content/post-date.php
@@ -7,5 +7,5 @@ $suffix = get_sub_field('suffix');
 ?>
 
 <div>
-	<p><span class="date-prefix"><?= $prefix; ?></span> <?= get_the_date($format); ?> <span class="date-suffix"><?= $suffix; ?></span></p>
+	<p><span class="date-prefix"><?= $prefix; ?></span> <span class="date"><?= get_the_date($format); ?></span> <span class="date-suffix"><?= $suffix; ?></span></p>
 </div>
