a81d4eb0f84e26247b42b7ee20dee9a49cc1fb4f
Cayden Simler <simler.cayden@gmail.com>
2021-11-10 14:00:42 -0800

    added query type selection to Post Query layout
    Query type selection allows you to choose how to build your query by presenting options for the following: Manual Selection, Selecting Post Type, Selecting Category(s), or Selecting Tag(s).  Other miscellaneous fixes include optimizations of opening/closing PHP tags, adding wp_reset_query() function to avoid issues, and optimizing the $postArguments variable to incorporate into the new options.

--- acf/layouts/post-query.php
+++ acf/layouts/post-query.php
@@ -41,53 +41,70 @@
     $wrapperClasses = rtrim($wrapperClasses) . '"';
 endwhile;
 
+$queryType = get_sub_field('query_type');
+$postArguments = [];
+
+if ($queryType !== 'choose') {
+    $queryAmount = get_sub_field('post_query_amount');
+    if ($queryAmount === 0) { $queryAmount = -1; }
+
+    $postArguments['posts_per_page'] = $queryAmount; 
+} else {
+    $selectedPosts = get_sub_field('select_posts');
+
+    $postArguments['orderby'] = 'post__in';
+    $postArguments['post__in'] = $selectedPosts;
+}
+
+if ($queryType === 'post-type') { 
+    $postArguments['post_type'] = get_sub_field('post_type'); 
+} else { $postArguments['post_type'] = 'any'; }
+
+if ($queryType === 'category') {
+    $postArguments['category__in'] = get_sub_field('post_query_category');
+    $postArguemnts['orderby'] = 'category__in';
+} elseif ($queryType === 'tags') {
+    $postArguments['tag__in'] = get_sub_field('post_query_tag');
+    $postArguemnts['orderby'] = 'tag__in';
+}  
+
+$postQuery = new WP_Query($postArguments);
 $queryContent = get_sub_field('post_query_content');
-
-$queryAmount = get_sub_field('query_amount');
-if (get_sub_field('query_amount') === 0) { $queryAmount = -1; }
-
-$postArguments = '';
-$postArguments = array(
-    'post_type' => get_sub_field('post_type'),
-    'posts_per_page' => $queryAmount, 
-);
 
 $topLevelLinked = get_sub_field('top_level_linked');
 $featuredImageBackground = get_sub_field('featured_image_background');
 
-if (get_sub_field('featured_image_size')) { $imageSize = get_sub_field('featured_image_size'); }
-
-$postQuery = new WP_Query($postArguments); ?>
-
+if (get_sub_field('featured_image_size')) { $imageSize = get_sub_field('featured_image_size'); } ?>
 
 <div <?php echo $layoutID; ?> <?php echo $wrapperClasses; ?>>
+    <?php $itemNumber = 1;
 
-    <?php $itemNumber = 1; ?>
-
-    <?php if ($gridType === 'masonry'): ?>
-        <?php for ($i = 1; $i <= $containerCount; $i++): ?>
+    if ($gridType === 'masonry'):
+        for ($i = 1; $i <= $containerCount; $i++): ?>
             <div class="layout-item col-<?php echo $i; ?>"></div>
-        <?php endfor; ?>
-    <?php endif;
+        <?php endfor;
+    endif;
 
-    while ( $postQuery->have_posts() ) : $postQuery->the_post(); ?>
-        <?php $imageBackground = ''; $imageBackgroundClass = ''; ?>
-        <?php if ($featuredImageBackground && get_the_post_thumbnail_url(get_the_ID(), $imageSize) && !in_array('image', $queryContent)):
-            $imageBackground = 'style="background-image:url(' . get_the_post_thumbnail_url(get_the_ID(), $imageSize) . ');"';
-            $imageBackgroundClass = ' post-image-background post-image-' . $imageSize;
-        endif; ?>
+    if ($postQuery->have_posts()):
+        while ( $postQuery->have_posts() ) : $postQuery->the_post();
+            $imageBackground = ''; $imageBackgroundClass = '';
 
-        <div <?php echo $contentClasses; ?>>
-            <div class="item-<?php echo $itemNumber; ?><?php echo $imageBackgroundClass; ?>" <?php echo $imageBackground; ?>>
+            if ($featuredImageBackground && get_the_post_thumbnail_url(get_the_ID(), $imageSize) && !in_array('image', $queryContent)):
+                $imageBackground = 'style="background-image:url(' . get_the_post_thumbnail_url(get_the_ID(), $imageSize) . ');"';
+                $imageBackgroundClass = ' post-image-background post-image-' . $imageSize;
+            endif; ?>
 
-                <?php foreach($queryContent as $contentType): ?>
-                    <?php echo '<div class="post-' . $contentType . '">'; ?>
-                        <?php get_template_part('acf/content/post-' . $contentType); ?>
-                    <?php echo '</div>'; ?>
-                <?php endforeach; ?>
+            <div <?php echo $contentClasses; ?>>
+                <div class="item-<?php echo $itemNumber; ?><?php echo $imageBackgroundClass; ?>" <?php echo $imageBackground; ?>>
 
-            </div>
-        </div>
-        <?php $itemNumber++; ?>
-    <?php endwhile;  ?>
+                    <?php foreach($queryContent as $contentType):
+                        echo '<div class="post-' . $contentType . '">';
+                            get_template_part('acf/content/post-' . $contentType);
+                        echo '</div>';
+                    endforeach; ?>
+
+                </div>
+            </div><?php $itemNumber++; ?>
+        <?php endwhile; wp_reset_query(); ?>
+    <?php else: echo 'No posts found.'; ?><?php endif; ?>
 </div>
