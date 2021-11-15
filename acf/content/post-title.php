1bb7bbccda5f55141ced66933f7bff83ad066665
Cayden Simler <simler.cayden@gmail.com>
2021-11-14 21:22:25 -0800

    included option for title link to post query layout

--- acf/content/post-title.php
+++ acf/content/post-title.php
@@ -4,12 +4,20 @@
 	endwhile; 
 else:
 	$settingsArray = generateBasicContent();
-endif; ?>
+endif; 
 
-<?php $textType = get_sub_field('type'); ?>
+$textType = get_sub_field('type'); 
+$titleLinks = get_sub_field('title_links'); ?>
 
 <div <?php echo $settingsArray['wrapper-classes']; ?> <?php echo $settingsArray['data-attributes']; ?>>
-	<<?php echo $textType; ?> <?php echo $settingsArray['content-classes']; ?>>
-		<span><?php the_title(); ?></span>
-	</<?php echo $textType; ?>>
+	<?php if ($titleLinks): ?>
+		<?php $postLink = get_permalink(); ?>
+		<a href="<?php echo $postLink; ?>" title="View the post titled '<?php the_title(); ?>'.">
+	<?php endif; ?>
+
+		<<?php echo $textType; ?> <?php echo $settingsArray['content-classes']; ?>>
+			<span><?php the_title(); ?></span>
+		</<?php echo $textType; ?>>
+
+	<?php if ($titleLinks): ?></a><?php endif; ?>
 </div>
