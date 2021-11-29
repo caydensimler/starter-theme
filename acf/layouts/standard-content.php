0f63e202b177c0fb2d32f312267f50a2df9ec5af
Cayden Simler <simler.cayden@gmail.com>
2021-11-29 12:53:49 -0800

    removed opening/closing php tags to organize code

--- acf/layouts/standard-content.php
+++ acf/layouts/standard-content.php
@@ -1,39 +1,30 @@
-<?php $layoutArray = generateBasicLayout();
-$itemNumber = 1; ?>
+<?php
+$layoutArray = generateBasicLayout();
 
-<?php if (have_rows('layout_item')): ?>
-	<div <?php echo $layoutArray['wrapper-classes']; ?>>
+if (have_rows('layout_item')) { $itemNumber = 1;
+	echo '<div ' . $layoutArray['wrapper-classes'] . '>';
 
-		<?php while(have_rows('layout_item')) : the_row(); ?>
+	while (have_rows('layout_item')) { the_row();
+		echo '<div ' .  $layoutArray['content-classes'] . '>';
+			echo '<div class="item">';
 
-				<div <?php echo $layoutArray['content-classes']; ?>>
-					<div class="item">
-						<?php if (have_rows('content_type')) { while (have_rows('content_type')) { the_row(); ?>
+				if (have_rows('content_type')) { while (have_rows('content_type')) { the_row();
+					$contentType = get_row_layout();
 
-							<?php 
-								$contentType = get_row_layout();
+					if ($contentType == 'wrapper') {
+						get_template_part('acf/content/' . $contentType);
+					} elseif ($contentType == 'wrapper-end') {
+						echo '</div></div>';
+					} else {
+						echo '<div class="' . $contentType . 'item-' . $itemNumber . '">';
+							get_template_part('acf/content/' . $contentType);
+						echo '</div>'; // close content type wrapper
+					}
+				}} // endif endwhile (have_rows('content_type'))
 
-								// Check if it's custom content and, if so, remove the custom- prefix from the content type.
-								if (substr($contentType, 0, strlen('custom-')) == 'custom-') {
-									$contentType = str_replace('custom-', '', $contentType);
-								}
-							?>
+			echo '</div>'; // close layout item container
+		echo '</div>'; // close layout item wrapper
 
-							<?php if ($contentType == 'wrapper'): ?>
-								<?php get_template_part('acf/content/' . $contentType); ?>
-							<?php elseif ($contentType == 'wrapper-end'): ?>
-								<?php echo '</div></div>'; ?>
-							<?php else: ?>
-								<div class="<?php echo $contentType; ?> item-<?php echo $itemNumber; ?>">
-									<?php get_template_part('acf/content/' . $contentType); ?>
-								</div>
-							<?php endif; ?>
-
-						<?php }} ?>
-					</div>
-				</div>
-			
-			<?php $itemNumber++; ?>
-		<?php endwhile ?>
-	</div>
-<?php endif; ?>
+		$itemNumber ++;
+	} // endwhile (have_rows('layout_item'))
+} // endif (have_rows('layout_item'))
